556e527b8d473aebfbada0ac726cdbf0a7f14f3d
matfrac <mateusz.p.frackowiak@gmail.com>
2021-12-23 21:23:03 +0100

    socket intro

--- Client/find-all-words/src/Window.java
+++ Client/find-all-words/src/Window.java
@@ -4,20 +4,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Window extends JFrame implements ActionListener {
+    private final JPanel cards;
+
+
     public Window() {
         super("FindAllWords");
-        this.setBackground(Color.LIGHT_GRAY);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(new Dimension(600,600));
+        this.setSize(new Dimension(1024, 720));
         this.setLocationRelativeTo(null);
+
+        this.cards = new JPanel(new CardLayout());
+        this.cards.setBackground(Color.LIGHT_GRAY);
+        this.add(this.cards);
+
         this.setVisible(true);
     }
 
 
-
     @Override
-    public void actionPerformed (ActionEvent e)
-    {
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
